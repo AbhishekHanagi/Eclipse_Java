b3cc7f293f4046064a0dede5b6b98c9a29d6376f
HP <abhishekhanagi.xworkz@gmail.com>
2022-08-13 23:21:38 +0530

    avc

---
+++ abc/src/com/xworkz/abc/abc.java
@@ -0,0 +1,5 @@
+package com.xworkz.abc;
+
+public class abc {
+
+}
---
+++ xyz/src/com/xworkz/xyz/aaa.java
@@ -0,0 +1,10 @@
+package com.xworkz.xyz;
+
+public class aaa {
+
+	public static void main(String[] args) {
+	System.out.println("hello");
+
+	}
+
+}
