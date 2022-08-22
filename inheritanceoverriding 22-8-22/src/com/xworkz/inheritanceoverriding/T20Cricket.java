22d02badbbc26d760be4f2a3f4f86809f6299860
unknown <abhishekhanagi.xworkz@gmail.com>
2022-08-23 00:08:30 +0530

    Over Riding

---
+++ inheritanceoverriding 22-8-22/src/com/xworkz/inheritanceoverriding/T20Cricket.java
@@ -0,0 +1,17 @@
+package com.xworkz.inheritanceoverriding;
+
+public class T20Cricket extends Cricket 
+{
+	public String manOfMatch() 
+	{
+		System.out.println("man of the match in T20Cricket");
+		return "Virat kohli";
+	}
+
+	@Override
+	public boolean entertainment() 
+	{
+		System.out.println("is there entertainment in T20Cricket");
+		return true;
+	}
+}
---
+++ inheritanceoverriding 22-8-22/src/com/xworkz/inheritanceoverriding/TestCricket.java
@@ -0,0 +1,19 @@
+package com.xworkz.inheritanceoverriding;
+
+public class TestCricket extends Cricket
+{
+	public String groundType;
+	
+	public void setGroundType(String groundType) 
+	{
+		this.groundType=groundType;
+		System.out.println("type of ground in TestCricket");
+	}
+	
+	@Override
+	public double presentation() 
+	{
+		System.out.println("presentation in TestCricket");
+		return 1500000;
+	}
+}
